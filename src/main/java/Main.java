f7e86ea2033b8573d30b2ed3d872eb6d21631d35
Alex <kertazzz@gmail.com>
2021-10-30 01:44:59 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "F:\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+
+        WebElement search = driver.findElement(By.xpath("//input[@aria-label='Найти']"));
+        search.sendKeys("Selenium", Keys.ENTER);
+        WebElement results = driver.findElement(By.id("result-stats"));
+        System.out.println(results.getAttribute("innerText"));
+
+
+    }
+}
